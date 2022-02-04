e3c021959c03db3eb7b15c167c0eda07a1e0bf7b
Excaliodus <98587691+Excaliodus@users.noreply.github.com>
2022-02-04 15:42:46 +0500

    Add files via upload

---
+++ 21L-7519-Assignment4.cpp
@@ -0,0 +1,20 @@
+//#include<iostream>
+//#include<fstream>
+//#include<string>
+//using namespace std;
+//int main()
+//{
+//	fstream file;
+//	file.open("data.txt", ios::out);
+//	file << "Hello World.\nTest File1." << endl;
+//	file.close();
+//	string myText;
+//	ifstream MyReadFile("data.txt");
+//	while (getline(MyReadFile, myText)) 
+//	{
+//		cout << myText << endl;
+//	}
+//	MyReadFile.close();
+//	system("pause");
+//	return 0;
+//}
